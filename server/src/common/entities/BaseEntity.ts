a3f5a5257384f8d0b719105c0c79a1261fba8e02
Nikita Khodakovsky <bednayadusha@gmail.com>
2022-05-04 20:28:46 +0300

    Server: Create Task SDL type

--- server/src/common/entities/BaseEntity.ts
+++ server/src/common/entities/BaseEntity.ts
@@ -1,13 +1,18 @@
 import { Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm'
+import { Field, ObjectType, ID } from 'type-graphql'
 
 @Entity()
+@ObjectType()
 export abstract class BaseEntity {
+	@Field(() => ID)
 	@PrimaryGeneratedColumn()
 	id: number
 
+	@Field()
 	@CreateDateColumn({ name: 'created_at' })
 	createdAt: Date
 
+	@Field()
 	@UpdateDateColumn({ name: 'updated_at' })
 	updatedAt: Date
 }
--- server/src/modules/task/entities/TaskEntity.ts
+++ server/src/modules/task/entities/TaskEntity.ts
@@ -1,11 +1,15 @@
 import { Entity, Column } from 'typeorm'
+import { ObjectType, Field } from 'type-graphql'
 import { BaseEntity } from '../../../common/entities'
 
 @Entity()
+@ObjectType()
 export class Task extends BaseEntity {
+	@Field()
 	@Column()
 	title: string
 
+	@Field()
 	@Column({ default: false })
 	isCompleted: boolean
 }
