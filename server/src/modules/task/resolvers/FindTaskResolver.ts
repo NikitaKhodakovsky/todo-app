0a7fb1417b61515f376355060b9d7ccb24494d5a
Nikita Khodakovsky <bednayadusha@gmail.com>
2022-05-04 21:59:44 +0300

    Server: Add findTaskById to FindTaskResolver

--- server/src/modules/task/resolvers/FindTaskResolver.ts
+++ server/src/modules/task/resolvers/FindTaskResolver.ts
@@ -1,4 +1,5 @@
-import { Resolver, Query, Ctx } from 'type-graphql'
+import { Resolver, Query, Ctx, Arg, Int } from 'type-graphql'
+import { TaskNotFoundError } from '../../../common/errors'
 import { Context } from '../../../types'
 import { Task } from '../entities'
 
@@ -7,5 +8,14 @@ export class FindTaskResolver {
 	@Query(() => [Task])
 	async tasks(@Ctx() ctx: Context): Promise<Task[]> {
 		return ctx.taskRepository.find()
+	}
+
+	@Query(() => Task)
+	async task(@Arg('id', () => Int) id: number, @Ctx() ctx: Context): Promise<Task> {
+		const task = await ctx.taskRepository.findOne({ where: { id } })
+
+		if (!task) throw new TaskNotFoundError()
+
+		return task
 	}
 }
