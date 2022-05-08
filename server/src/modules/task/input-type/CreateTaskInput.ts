4242d8e9a2a9edc4bfa0134d7103fde500ef2c5c
Nikita Khodakovsky <bednayadusha@gmail.com>
2022-05-08 23:10:55 +0300

    Server: Add the ability to create a completed task

--- server/src/modules/task/input-type/CreateTaskInput.ts
+++ server/src/modules/task/input-type/CreateTaskInput.ts
@@ -4,4 +4,7 @@ import { InputType, Field } from 'type-graphql'
 export class CreateTaskInput {
 	@Field()
 	title: string
+
+	@Field({ nullable: true })
+	isCompleted: boolean
 }
--- server/src/modules/task/resolvers/CreateTaskResolver.ts
+++ server/src/modules/task/resolvers/CreateTaskResolver.ts
@@ -7,8 +7,8 @@ import { Task } from '../entities'
 @Resolver()
 export class CreateTaskResolver {
 	@Mutation(() => Task)
-	async createTask(@Arg('data') { title }: CreateTaskInput, @Ctx() ctx: Context): Promise<Task> {
-		const task = ctx.taskRepository.create({ title })
+	async createTask(@Arg('data') { title, isCompleted }: CreateTaskInput, @Ctx() ctx: Context): Promise<Task> {
+		const task = ctx.taskRepository.create({ title, isCompleted })
 		return ctx.taskRepository.save(task)
 	}
 }
