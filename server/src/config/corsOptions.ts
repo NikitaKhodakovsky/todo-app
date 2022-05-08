a78b484c4fd90821ad9c6d0e6e4340449421db40
Nikita Khodakovsky <bednayadusha@gmail.com>
2022-05-08 22:02:01 +0300

    Server: Add the ability to provide multiple origins

--- server/src/config/corsOptions.ts
+++ server/src/config/corsOptions.ts
@@ -1,6 +1,27 @@
 import { CorsOptions } from 'cors'
+import { red } from 'ansi-colors'
+
+function getWhiteList(origin?: string): string[] {
+	if (!origin) {
+		console.error(red('Invalid origin environment variable'))
+		return ['*']
+	}
+
+	return origin
+		.split(' ')
+		.map((url) => url.trim())
+		.filter((i) => i.length > 1) //if origins divided by more than one space trim() creates empty strings
+}
+
+const whiteList: string[] = getWhiteList(process.env.ORIGIN)
 
 export const corsOptions: CorsOptions = {
-	origin: process.env.ORIGIN,
-	credentials: true
+	credentials: true,
+	origin: function (origin, callback) {
+		if (origin && whiteList.indexOf(origin) !== -1) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	}
 }
