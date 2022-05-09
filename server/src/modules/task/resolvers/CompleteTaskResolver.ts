12a0065772b4ccfccf0bb0e3777cb9fe2584bbbb
Nikita Khodakovsky <bednayadusha@gmail.com>
2022-05-09 19:07:29 +0300

    Server: Change id argument type from Int to ID in FindTaskByIdResolver, DeleteTaskResolver and CompleteTaskResolver

--- server/src/modules/task/resolvers/CompleteTaskResolver.ts
+++ server/src/modules/task/resolvers/CompleteTaskResolver.ts
@@ -1,4 +1,4 @@
-import { Arg, Ctx, Int, Mutation, Resolver } from 'type-graphql'
+import { Arg, Ctx, ID, Mutation, Resolver } from 'type-graphql'
 import { TaskNotFoundError } from '../../../common/errors'
 import { Context } from '../../../types'
 import { Task } from '../entities'
@@ -6,7 +6,7 @@ import { Task } from '../entities'
 @Resolver()
 export class CompleteTaskResolver {
 	@Mutation(() => Task)
-	async completeTask(@Arg('id', () => Int) id: number, @Ctx() ctx: Context): Promise<Task> {
+	async completeTask(@Arg('id', () => ID) id: number, @Ctx() ctx: Context): Promise<Task> {
 		await ctx.taskRepository.update(id, { isCompleted: true })
 
 		const task = await ctx.taskRepository.findOne({ where: { id } })
--- server/src/modules/task/resolvers/FindTaskByIdResolver.ts
+++ server/src/modules/task/resolvers/FindTaskByIdResolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx, Arg, Int } from 'type-graphql'
+import { Resolver, Query, Ctx, Arg, ID } from 'type-graphql'
 import { TaskNotFoundError } from '../../../common/errors'
 import { Context } from '../../../types'
 import { Task } from '../entities'
@@ -6,7 +6,7 @@ import { Task } from '../entities'
 @Resolver()
 export class FindTaskByIdResolver {
 	@Query(() => Task)
-	async task(@Arg('id', () => Int) id: number, @Ctx() ctx: Context): Promise<Task> {
+	async task(@Arg('id', () => ID) id: number, @Ctx() ctx: Context): Promise<Task> {
 		const task = await ctx.taskRepository.findOne({ where: { id } })
 
 		if (!task) throw new TaskNotFoundError()
