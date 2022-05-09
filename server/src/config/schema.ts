5f65b4ef50512c4a84eaaeb7b87535612166de6b
Nikita Khodakovsky <bednayadusha@gmail.com>
2022-05-09 20:06:34 +0300

    Server: Rewrite the resolvers registration mechanism for more modularity

--- server/src/config/schema.ts
+++ server/src/config/schema.ts
@@ -1,25 +1,9 @@
 import { buildSchemaSync } from 'type-graphql'
 
-import {
-	ActiveTasksCountResolver,
-	ClearCompletedResolver,
-	ToggleTaskStatusResolver,
-	CreateTaskResolver,
-	DeleteTaskResolver,
-	FindAllTasksResolver,
-	FindTaskByIdResolver
-} from '../modules/task'
+import { TaskResolvers } from '../modules/task'
 
 export const schema = buildSchemaSync({
-	resolvers: [
-		FindAllTasksResolver,
-		FindTaskByIdResolver,
-		CreateTaskResolver,
-		ToggleTaskStatusResolver,
-		DeleteTaskResolver,
-		ClearCompletedResolver,
-		ActiveTasksCountResolver
-	],
+	resolvers: [...TaskResolvers],
 	dateScalarMode: 'isoDate',
 	emitSchemaFile: true
 })
--- server/src/modules/task/resolvers/index.ts
+++ server/src/modules/task/resolvers/index.ts
@@ -1,7 +1,18 @@
-export * from './FindAllTasksResolver'
-export * from './FindTaskByIdResolver'
-export * from './CreateTaskResolver'
-export * from './ToggleTaskStatusResolver'
-export * from './DeleteTaskResolver'
-export * from './ClearCompletedResolver'
-export * from './ActiveTasksCountResolver'
+import { BuildSchemaOptions } from 'type-graphql'
+import { ActiveTasksCountResolver } from './ActiveTasksCountResolver'
+import { ClearCompletedResolver } from './ClearCompletedResolver'
+import { CreateTaskResolver } from './CreateTaskResolver'
+import { DeleteTaskResolver } from './DeleteTaskResolver'
+import { FindAllTasksResolver } from './FindAllTasksResolver'
+import { FindTaskByIdResolver } from './FindTaskByIdResolver'
+import { ToggleTaskStatusResolver } from './ToggleTaskStatusResolver'
+
+export const TaskResolvers: BuildSchemaOptions['resolvers'] = [
+	FindAllTasksResolver,
+	FindTaskByIdResolver,
+	CreateTaskResolver,
+	ToggleTaskStatusResolver,
+	DeleteTaskResolver,
+	ClearCompletedResolver,
+	ActiveTasksCountResolver
+]
