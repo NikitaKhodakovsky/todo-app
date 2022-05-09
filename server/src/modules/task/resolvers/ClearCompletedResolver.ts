a081696326f541b27bc7a268a2a96f6d86bb210d
Nikita Khodakovsky <bednayadusha@gmail.com>
2022-05-09 20:16:54 +0300

    Server: Change ClearCompletedResolver return type to Boolean

--- server/src/modules/task/resolvers/ClearCompletedResolver.ts
+++ server/src/modules/task/resolvers/ClearCompletedResolver.ts
@@ -1,12 +1,11 @@
 import { Ctx, Mutation, Resolver } from 'type-graphql'
 import { Context } from '../../../types'
-import { Task } from '../entities'
 
 @Resolver()
 export class ClearCompletedResolver {
-	@Mutation(() => [Task])
-	async clearCompleted(@Ctx() ctx: Context): Promise<Task[]> {
+	@Mutation(() => Boolean)
+	async clearCompleted(@Ctx() ctx: Context): Promise<boolean> {
 		await ctx.taskRepository.delete({ isCompleted: true })
-		return ctx.taskRepository.find()
+		return true
 	}
 }
